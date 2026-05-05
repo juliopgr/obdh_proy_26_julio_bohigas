a7b64f67a8062af635aefe03ff65226df540c903
juliopgr <158303997+juliopgr@users.noreply.github.com>
2026-05-05 19:28:55 +0200

    FIX rename transicion todo2

--- DJF/ITs/IT_ASW_CCDrineMng/~/ClasesDato/CDTCExecCtrl.h
+++ DJF/ITs/IT_ASW_CCDrineMng/~/ClasesDato/CDTCExecCtrl.h
@@ -1,8 +1,10 @@
 #ifndef FCDTCExecCtrlH
 #define FCDTCExecCtrlH
 
+
+//TODO 11 Part 1 DONE Add ExecCtrlDroneTC as a new type of TC in the enum TTCExecCtrl
 enum TTCExecCtrl {
-	ExecCtrlPrioTC, ExecCtrlReboot, ExecCtrlHK_FDIRTC, ExecCtrlBKGTC
+	ExecCtrlPrioTC, ExecCtrlReboot, ExecCtrlHK_FDIRTC, ExecCtrlBKGTC, ExecCtrlDroneTC
 };
 
 class CDTCExecCtrl{
@@ -29,7 +31,7 @@ class CDTCExecCtrl{
 	 * \return true if it is a priority telecommand
 	 *
 	 */
-	//TODO 
+
 	bool IsPrioTC(){return (ExecCtrlPrioTC==mExecCtrl);}
 
 	/**
@@ -48,7 +50,19 @@ class CDTCExecCtrl{
 	 *
 	 */
 	bool IsBKGTC() {return (ExecCtrlBKGTC==mExecCtrl);};
-	
+
+	//TODO 11 Part 2 DONE Define the codeo of IsDroneTC() to TTCExecCtrl
+
+	/**
+	 * \brief Check if is a Drone telecommand
+	 *
+	 * \return true if it is a Drone telecommand
+	 *
+	 */
+	bool IsDroneTC() {return (ExecCtrlDroneTC==mExecCtrl);}
 	
 };
+
+//TODO 11 Part 3 DONE Copy this file in the EDROOM Model Data Class CDTCExecCtrl
+
 #endif
